ba04fb2b22823870f7caf5e543bcd26a1f27b76b
Mike <63655075+MixelTe@users.noreply.github.com>
2022-05-03 20:09:09 +0300

    new formulas

--- ts/formula.ts
+++ ts/formula.ts
@@ -59,6 +59,11 @@ class FormulaBuilder
 	public l(letter: keyof typeof formulaLetters)
 	{
 		const letterEl = formulaLetters[letter];
+		if (!letterEl)
+		{
+			this.t(letter);
+			return this
+		}
 		if (letterEl.d && letterEl.vb)
 		{
 			const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -132,9 +137,10 @@ const formulaLetters = {
 	"V": { ch: "ν", dy: 0, vb: "-3 -5 6 5", d: "M-2.718-3.761C-2.392-4.783-1.907-4.804-1.528-3.993L0 0 1.526-3.982C1.884-4.741 2.41-4.709 2.715-3.719" },
 	"p": { ch: "ρ", dy: 0, vb: "0 -5 5 5", d: "M.283-1.392C.43-.444 1.294-.065 1.82-1.234L2.663-3.961A1 1 0 112.705-3.256" },
 	"m": { ch: "μ", dy: 0.1, vb: "0 -4.5 5 4.5", d: "M.256-.799C.272-.085.878.16 1.308-1.26L1.957-3.772C1.993-3.92 2.039-3.914 2.053-3.826L2.513-1.92Q2.69-1.114 2.982-1.789L3.925-3.855C4.024-4.039 4.029-3.891 4.032-3.856L4.202-1.805Q4.294-1.037 4.862-1.582" },
+	"n": { ch: "η", dy: 0, vb: "0 -5.5 5 5.6", d: "M.3-2C.2 0 2.3 1.1 2.8-2 2.9-2.5 3-5.1 2.1-5.3" },
+	"a": { ch: "ⲁ", dy: 0, vb: "-0.3 -4.5 5 5.1", d: "M3.5-3.7C3.5-2.3 2.3-1.3 1.3-.3.7.3-.2-.5.4-1.1 1.1-1.6 1.2-.7 1.8-.4 2.6.1 3.6 0 3.9-.4" },
 	"d": { ch: "Δ", dy: 0, vb: null, d: null },
 	"P": { ch: "π", dy: 0, vb: null, d: null },
-	"n": { ch: "η", dy: 0, vb: null, d: null },
 	"l": { ch: "λ", dy: 0, vb: null, d: null },
 }
 
